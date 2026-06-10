8b5d811ebda346c332a384d3d975e38d273d194a
Marie Lopez <lopezmariegabrielle@gmail.com>
2026-06-10 10:19:07 +0200

    Add task

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -10,6 +10,9 @@ Base = declarative_base()
 
 
 class State(Base):
+    """
+    Represents a state for a MySQL database.
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
