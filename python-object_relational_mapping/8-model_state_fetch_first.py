7739e974e877bd3cd96eeda99c460a2a75dbea95
Marie Lopez <lopezmariegabrielle@gmail.com>
2026-06-10 11:02:13 +0200

    Add task

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -8,7 +8,7 @@ from model_state import Base, State
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
-    detabase_name = sys.argv[3]
+    database_name = sys.argv[3]
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost:3306/{}'
         .format(username, password, database_name),
